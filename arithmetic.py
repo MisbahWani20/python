df8cd66ebc8243ede793073a910096b0b78c13f8
MisbahWani20 <misbahwani62gmail.com>
2024-10-31 20:31:58 +0530

    arithmetic operations

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+#addition
+a = input("enter num1:")
+b = input("enter num2:")
+sum = int(a) + int(b)
+print("Addition is ", sum)
+
+#subtraction
+sub = int(a) - int(b)
+print("Subtraction is ", sub)
+
+#multiplication
+mul = int(a) * int(b)
+print("Multiplication is ", mul)
+
+#division
+div = int(a) / int(b)
+print("Division is ", div)
