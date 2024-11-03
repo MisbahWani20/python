45d65efafd70508f5c6f30d98415c69b4683011e
MisbahWani20 <misbahwani62gmail.com>
2024-11-03 12:03:32 +0530

    updated

--- armstrong.py
+++ armstrong.py
@@ -4,9 +4,8 @@ temp = n
 while (temp > 0):
     a = temp % 10
     sum = sum + (a*a*a)
-    temp = temp / 10
-print(sum)
-#if n == sum:
- #   print('Armstrong number')
-#else:
- #   print('Not an armstrong')
+    temp = temp // 10
+if n == sum:
+    print('Armstrong number')
+else:
+    print('Not an armstrong')
